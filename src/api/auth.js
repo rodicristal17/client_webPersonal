e4a15db7e8146b97b0cd32d2a3550f8e60e94ae8
rodicristal17 <rodicristal17@gmail.com>
2023-03-10 14:22:32 -0300

    first commit

---
+++ src/api/auth.js
@@ -0,0 +1,38 @@
+import { ENV } from "../utils";
+
+export class Auth {
+    baseApi = ENV.BASE_API;
+
+    async register(data) {
+        try {
+            const url = `${this.baseApi}/${ENV.API_ROUTES.REGISTER}`;
+
+            const params = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: data.email,
+                    password: data.password
+                }),
+            }
+
+            console.log(url);
+            console.log(params);
+
+            
+
+            const response = await fetch(url, params);
+            
+            console.log(response.status);
+
+            const result = await response.json();
+            if (response.status !== 200) throw result;
+
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
